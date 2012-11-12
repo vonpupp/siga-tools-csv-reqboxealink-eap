a06aeec7508f4593433cff08b10a7a5436729fd4
Albert De La Fuente <vonpupp@gmail.com>
2012-11-12 14:21:59 -0200

    dicts are correctly mapped

--- reqboxealink.py
+++ reqboxealink.py
@@ -58,42 +58,121 @@ class ReqBoxLinker():
    Attributes:
       - ea: Dispatch based COM object
    """
-   ea = None
+   eap = None
+   repo = None
+   eapfile = None
+   linksfile = None
    relmatrix = []
+   sdict = {}
+   ddict = {}
     
    def __init__(self):
       # Public
       # Init structures
       pass
-   
-   def getdispatcher(self):
-      try:
-         # Dispatch
-         ea = win32com.client.Dispatch('EA.Repository')
-      except:
-         print "failure dispatch"
-         sys.exit(2)
-        
-   def loadeap(self):
-      try:
-         # Open file
-         o.OpenFile2(self.eapfile, 1, 0)
-         #o.OpenFile2('/cygdrive/c/siga-tools-ea-relation/SIGA.EAP', 1, 0)
-         #o.OpenFile2('SIGA.EAP', 1, 0)
-      except:
-         print "failure opening file"
-         sys.exit(2)
-   
+
    def loadlinks(self):
       f = codecs.open(self.linksfile, encoding='utf-8', mode='r')
       reader = csv.reader(f, delimiter='\t')
       self.relmatrix = []
-      reader.next()
+      #reader.next()
       for row in reader:
          self.relmatrix += [row]
+      
+   def sourcens(self):
+      result = self.relmatrix[0][0]
+      return result
 
-   def loadeapa(self):
+   def destinationns(self):
+      result = self.relmatrix[0][1]
+      return result
+
+   def getdispatcher(self):
+      try:
+         # Dispatch
+         self.eap = win32com.client.Dispatch('EA.Repository')
+      except:
+         print("COM dispatch error: EA.Repository")
+         sys.exit(2)
+        
+   def loadeap(self):
+#      try:
+         # Open file
+         self.eap.OpenFile2(self.eapfile, 1, 0)
+         #self.eap.OpenFile2('/cygdrive/c/siga-tools-ea-relation/SIGA.EAP', 1, 0)
+         #self.eap.OpenFile2('SIGA.EAP', 1, 0)
+      #except:
+      #   print "failure opening file"
+      #   sys.exit(2)
+   
+   def nspackagefetch(self, nsstring, dictstruc):
+      # Example:
+      # SIGA stable|Biblioteca REQ..|Requisitos FI|Comum
+      
+      ns = nsstring.split('|')
+      print("  Opening model: %s" % ns[0]);
+      package = self.eap.Models.GetByName(ns[0]);
+      for i in range(1, len(ns)):
+         print("  Opening package: %s" % ns[i]);
+         package = package.Packages.GetByName(ns[i])
+         
+      for i in xrange(0, package.Elements.Count):
+         element = package.Elements.GetAt(i)
+         print("    Mapping element: %s" % element.Name);
+         alias = element.Alias
+         dictstruc[alias] = element
       pass
+   
+   def linksourcetodestination(self):
+      for i in range(1, len(self.relmatrix)):
+#      sPackage = nsPackageFetch(pSourceNS, sMap);
+#      dPackage = nsPackageFetch(pDestinationNS, dMap);
+#
+#        for (i = 1; i < relMatrix.length; i++) {
+#            sElement = sMap.get(relMatrix[i][0]);
+#            dElement = dMap.get(relMatrix[i][1]);
+#            //out.printf("testing... %s vs. %s\n", relMatrix[i][0].substring(0, 6));
+#            //out.printf("testing... %s vs. %s\n", relMatrix[i][1].substring(0, 6));
+#            if (sElement == null){
+#                out.printf("SKIPPING (not found): %s on namespace: %s\n", relMatrix[i][0], pSourceNS);
+#            } else {
+#                out.printf("Source element found... %s\n", sElement.GetName());
+#                if (dElement == null){
+#                    out.printf("SKIPPING (not found): %s on namespace: %s\n", relMatrix[i][1], pDestinationNS);
+#                } else {
+#                    relsdname = relMatrix[i][2];
+#                    reltype   = relMatrix[i][3];
+#                    if (relMatrix[i][2].compareToIgnoreCase("auto") == 0 || 
+#                        relMatrix[i][2].equals("")) {
+#                        relsdname = "rel-" + relMatrix[i][0] + "-" +
+#                            relMatrix[i][1];
+#                        reldsname = "rel-" + relMatrix[i][1] + "-" +
+#                            relMatrix[i][0];
+#                    }
+#                    out.printf("Destination element found... %s\n", dElement.GetName());
+#                    
+#                    // Source -> Destination relation
+#                    out.printf("Doing the magic tricks: creating relation [%s] between [%s] and [%s] (type [%s])\n",
+#                        relsdname, sElement.GetName(), dElement.GetName(), reltype);
+#                    
+#                    sdcon = sElement.GetConnectors().AddNew(relsdname, reltype);
+#                    sdcon.SetSupplierID(dElement.GetElementID());
+#                    sdcon.Update();
+#                    sElement.Refresh();
+#                    
+#                    // Destination -> Source
+#//                    out.printf("Doing the magic tricks: creating relation [%s] between [%s] and [%s]\n",
+#//                        reldsname, dElement.GetName(), sElement.GetName());
+#//                    
+#//                    dscon = dElement.GetConnectors().AddNew(reldsname, "Realization");
+#//                    dscon.SetSupplierID(sElement.GetElementID());
+#//                    dscon.Update();
+#//                    dElement.Refresh();
+#                }
+#            }
+#        }
+#    }
+
 
 def main(argv):
    try:
@@ -113,15 +192,28 @@ def main(argv):
          sys.stdout.write(__doc__)
          return 0
       elif opt in ('-e', '--eap'):
-         rl.eafile = arg
+         rl.eapfile = arg
       elif opt in ('-l', '--links'):
          rl.linksfile = arg
       
       #rl.parseall = rb.parseall or opt in ('-a', '--export-all')
    
+   print("Loading: " + rl.linksfile + " relationship matrix...")
    rl.loadlinks()
+   
+   print("Loading: COM dispatch EA.Repository")
    rl.getdispatcher()
+   
+   print("Loading: " + rl.eapfile + " repository file")
    rl.loadeap()
+   
+   print("Loading: " + rl.sourcens() + " source namespace")
+   rl.nspackagefetch(rl.sourcens(), rl.sdict)
+   
+   print("Loading: " + rl.destinationns() + " destination namespace")
+   rl.nspackagefetch(rl.destinationns(), rl.ddict)
+
+   rl.linksourcetodestination()
    
 if __name__ == "__main__":
    sys.exit(main(sys.argv[1:]))
